1792a7ff0ed64c55cba98db8da2a78a1be466d1b
ivan-arteaga-tfgm <ivan.arteaga@tfgm.com>
2024-02-06 23:24:44 +0000

    fixing issues

--- lib/cdk-fargate-nodejs-stack.ts
+++ lib/cdk-fargate-nodejs-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Networking } from './network-stack';
+import { TaskDefinition } from 'aws-cdk-lib/aws-ecs';
 const path = require('path');
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -20,28 +21,28 @@ export class CdkFargateNodejsStack extends cdk.Stack {
     // Database implementation section
 
     // Create username and password secret for DB Cluster
-    const secret = new cdk.aws_rds.DatabaseSecret(this, 'AuroraSecret', {
-      username: 'clusteradmin',
-    });
+    // const secret = new cdk.aws_rds.DatabaseSecret(this, 'AuroraSecret', {
+    //   username: 'clusteradmin',
+    // });
 
-    const databasecluster = new cdk.aws_rds.DatabaseCluster(this, 'Database', {
-      engine: cdk.aws_rds.DatabaseClusterEngine.auroraMysql({
-        version: cdk.aws_rds.AuroraMysqlEngineVersion.VER_3_03_0,
-      }),
-      writer: cdk.aws_rds.ClusterInstance.provisioned('writer'),
-      vpc: vpcStack.vpc,
-    });
+    // const databasecluster = new cdk.aws_rds.DatabaseCluster(this, 'Database', {
+    //   engine: cdk.aws_rds.DatabaseClusterEngine.auroraMysql({
+    //     version: cdk.aws_rds.AuroraMysqlEngineVersion.VER_3_03_0,
+    //   }),
+    //   writer: cdk.aws_rds.ClusterInstance.provisioned('writer'),
+    //   vpc: vpcStack.vpc,
+    // });
 
-    const proxy = new cdk.aws_rds.DatabaseProxy(this, 'Proxy', {
-      proxyTarget: cdk.aws_rds.ProxyTarget.fromCluster(databasecluster),
-      secrets: [databasecluster.secret!],
-      vpc: vpcStack.vpc,
-    });
+    // const proxy = new cdk.aws_rds.DatabaseProxy(this, 'Proxy', {
+    //   proxyTarget: cdk.aws_rds.ProxyTarget.fromCluster(databasecluster),
+    //   secrets: [databasecluster.secret!],
+    //   vpc: vpcStack.vpc,
+    // });
 
-    const role = new cdk.aws_iam.Role(this, 'DBProxyRole', {
-      assumedBy: new cdk.aws_iam.AccountPrincipal(this.account),
-    });
-    proxy.grantConnect(role, 'admin');
+    // const role = new cdk.aws_iam.Role(this, 'DBProxyRole', {
+    //   assumedBy: new cdk.aws_iam.AccountPrincipal(this.account),
+    // });
+    // proxy.grantConnect(role, 'admin');
 
     // Database implementation section
 
@@ -58,86 +59,146 @@ export class CdkFargateNodejsStack extends cdk.Stack {
       vpc: vpcStack.vpc,
     });
 
-    const frontend =
-      new cdk.aws_ecs_patterns.ApplicationLoadBalancedFargateService(
-        this,
-        'MyFargateService',
-        {
-          cluster: ecscluster, // Required
-          cpu: 512, // Default is 256
-          desiredCount: 6, // Default is 1
-          taskImageOptions: {
-            // image: cdk.aws_ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
-            image: cdk.aws_ecs.ContainerImage.fromDockerImageAsset(
-              new cdk.aws_ecr_assets.DockerImageAsset(this, 'webserver', {
-                directory: path.join(__dirname, '../webserver'),
-              })
-            ),
+    cdk.Tags.of(ecscluster).add('Service', 'ECS-cluster');
+
+    // needs investigation to be used
+    // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ecs_patterns.ApplicationLoadBalancedFargateService.html
+    // const frontend =
+    //   new cdk.aws_ecs_patterns.ApplicationLoadBalancedFargateService(
+    //     this,
+    //     'FargateService',
+    //     {
+    //       cluster: ecscluster, // Required
+    //       circuitBreaker: {
+    //         rollback: true,
+    //       },
+    //       cpu: 512, // Default is 256
+    //       desiredCount: 2, // Default is 1
+    //       taskImageOptions: {
+    //         image: cdk.aws_ecs.ContainerImage.fromDockerImageAsset(
+    //           new cdk.aws_ecr_assets.DockerImageAsset(this, 'webserver', {
+    //             directory: path.join(__dirname, '../webserver'),
+    //           })
+    //         ),
+    //         containerPort: 8080,
+    //         logDriver: cdk.aws_ecs.LogDrivers.awsLogs({
+    //           streamPrefix: id,
+    //           logRetention: cdk.aws_logs.RetentionDays.ONE_YEAR,
+    //         }),
+    //       },
+    //       memoryLimitMiB: 512, // Default is 512
+    //       publicLoadBalancer: true, // Default is true
+    //       // securityGroups:
+    //     }
+    //   );
+
+    const taskDefinition = new cdk.aws_ecs.FargateTaskDefinition(
+      this,
+      'TaskDef'
+    );
+    // public addContainer(id: string, props: ContainerDefinitionOptions): ContainerDefinition
+
+    taskDefinition.addContainer('webserver', {
+      image: cdk.aws_ecs.ContainerImage.fromDockerImageAsset(
+        new cdk.aws_ecr_assets.DockerImageAsset(this, 'webserver', {
+          directory: path.join(__dirname, '../webserver'),
+        })
+      ),
+      cpu: 256,
+      memoryLimitMiB: 512,
+      logging: cdk.aws_ecs.LogDrivers.awsLogs({
+        // prefix-name/container-name/ecs-task-id
+        streamPrefix: '/aws/ecs',
+        logRetention: cdk.aws_logs.RetentionDays.ONE_YEAR,
+        // (logGroup: optional, default: A log group is automatically created.)
+        // logGroup: new cdk.aws_logs.LogGroup(this, 'fargatecluster', {
+        //   logGroupName: 'cdkIntegLogGroup',
+        // }),
+      }),
+      portMappings: [
+        { containerPort: 8080, protocol: cdk.aws_ecs.Protocol.TCP },
+      ],
+      // secrets:
+    });
+
+    console.log(TaskDefinition.fromTaskDefinitionAttributes.name);
+    // taskDefinition.findPortMappingByName('webserver');
+
+    //add private zone in route53
+    // ecscluster.addDefaultCloudMapNamespace({
+    //   name: 'webserver',
+    // });
+
+    const fargateservice = new cdk.aws_ecs.FargateService(
+      this,
+      'FargateService',
+      {
+        cluster: ecscluster,
+        taskDefinition,
+        // circuitBreaker: {
+        //   rollback: true,
+        // },
+        desiredCount: 2, // Default is 1
+        capacityProviderStrategies: [
+          {
+            capacityProvider: 'FARGATE_SPOT',
+            weight: 2,
           },
-          memoryLimitMiB: 2048, // Default is 512
-          publicLoadBalancer: true, // Default is true
-        }
-      );
+          {
+            capacityProvider: 'FARGATE',
+            weight: 1,
+          },
+        ],
+        assignPublicIp: true,
+        // SecurityGroup:'',
+      }
+    );
 
-    // const taskDefinition = new cdk.aws_ecs.FargateTaskDefinition(
-    //   this,
-    //   'TaskDef',
-    //   {
-    //     cpu: 1024,
-    //     taskRole: role,
-    //   }
-    // );
+    cdk.Tags.of(fargateservice).add('Service', 'ECS-fargate-service');
 
-    // taskDefinition.addContainer('webserver', {
-    //   image: cdk.aws_ecs.ContainerImage.fromDockerImageAsset(
-    //     new cdk.aws_ecr_assets.DockerImageAsset(this, 'webserver', {
-    //       directory: path.join(__dirname, '../webserver'),
-    //     })
-    //   ),
-    // });
+    const lb = new cdk.aws_elasticloadbalancingv2.ApplicationLoadBalancer(
+      this,
+      'ALB',
+      { vpc: vpcStack.vpc, internetFacing: true }
+    );
 
-    // const fargateServiceName = 'FargateService';
-    // const fargateservice = new cdk.aws_ecs.FargateService(
-    //   this,
-    //   fargateServiceName,
-    //   {
-    //     cluster: ecscluster,
-    //     taskDefinition,
-    //     capacityProviderStrategies: [
-    //       {
-    //         capacityProvider: 'FARGATE_SPOT',
-    //         weight: 2,
-    //       },
-    //       {
-    //         capacityProvider: 'FARGATE',
-    //         weight: 1,
-    //       },
-    //     ],
-    //   }
-    // );
+    const listener = lb.addListener('worldListener', { port: 80 });
+    fargateservice.registerLoadBalancerTargets({
+      containerName: 'webserver',
+      containerPort: 8080,
+      newTargetGroupId: 'ECSBlue',
+      listener: cdk.aws_ecs.ListenerConfig.applicationListener(listener, {
+        // protocol: cdk.aws_elasticloadbalancingv2.ApplicationProtocol.HTTPS,
+        protocol: cdk.aws_elasticloadbalancingv2.ApplicationProtocol.HTTP,
+      }),
+    });
 
-    // const cpuMetric = new cdk.aws_cloudwatch.Metric({
-    //   metricName: 'CPUUtilization',
-    //   namespace: 'AWS/ECS',
-    //   period: cdk.Duration.minutes(5),
-    //   statistic: 'Average',
-    //   dimensionsMap: {
-    //     ClusterName: ecscluster.clusterName,
-    //     // Using `service.serviceName` here will cause a circular dependency
-    //     ServiceName: fargateServiceName,
-    //   },
-    // });
+    // Monitoring implementation section
 
-    // const fargateAlarm = new cdk.aws_cloudwatch.Alarm(this, 'CPUAlarm', {
-    //   alarmName: 'cpuAlarmName',
-    //   metric: cpuMetric,
-    //   evaluationPeriods: 2,
-    //   threshold: 80,
-    // });
+    const cpuMetric = new cdk.aws_cloudwatch.Metric({
+      metricName: 'CPUUtilization',
+      namespace: 'AWS/ECS',
+      period: cdk.Duration.minutes(5),
+      statistic: 'Average',
+      dimensionsMap: {
+        ClusterName: ecscluster.clusterName,
+        // Using `service.serviceName` here will cause a circular dependency
+        ServiceName: fargateservice.cluster.clusterName,
+      },
+    });
 
-    // fargateservice.enableDeploymentAlarms([fargateAlarm.alarmName], {
-    //   behavior: cdk.aws_ecs.AlarmBehavior.FAIL_ON_ALARM,
-    // });
+    const fargateAlarm = new cdk.aws_cloudwatch.Alarm(this, 'CPUAlarm', {
+      alarmName: 'cpuAlarmName',
+      metric: cpuMetric,
+      evaluationPeriods: 2,
+      threshold: 80,
+    });
+
+    fargateservice.enableDeploymentAlarms([fargateAlarm.alarmName], {
+      behavior: cdk.aws_ecs.AlarmBehavior.FAIL_ON_ALARM,
+    });
+
+    // END Monitoring implementation section
 
     // const LambdaIntegration = new cdk.aws_apigateway.LambdaIntegration(Lambda, {
     //   allowTestInvoke: false,
--- webserver/src/app.ts
+++ webserver/src/app.ts
@@ -7,19 +7,23 @@ const apiBase = process.env.API_BASE;
 
 const app = express();
 
-app.set('views', path.join(__dirname, '..', 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, '..', 'views'));
+// app.set('view engine', 'ejs');
+
+// app.get('/', async (req, res) => {
+//   const documents = await getDocuments();
+//   res.render('index', { documents: documents });
+// });
 
 app.get('/', async (req, res) => {
-  const documents = await getDocuments();
-  res.render('index', { documents: documents });
+  res.send('Hello World from TFGM!');
 });
 
 app.listen(port, () => {
-  console.log(`Documents Management Web Server Started. Port: port`);
+  console.log(`Documents Management Web Server Started. Port: ${port}`);
 });
 
-const getDocuments = async () => {
-  const { data: results } = await axios.get(`${apiBase}getDocuments`);
-  return results;
-};
+// const getDocuments = async () => {
+//   const { data: results } = await axios.get(`${apiBase}getDocuments`);
+//   return results;
+// };
